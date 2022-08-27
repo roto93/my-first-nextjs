0abb3c4486e6afec1a105d990f5002feb45a35f1
roto93 <roto93@yahoo.com.tw>
2022-08-28 01:00:42 +0800

    :zap: load script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function FirstPost() {
 
@@ -9,6 +10,21 @@ export default function FirstPost() {
     {/* 如果要一次更改所有頁面的Head，或是要修改<html>，則需要用 _document.js */}
     <Head>
       <title>First Post</title>
+
+      {/* 用 next/script 提供的 <Script> 來讀取 script
+      好處是方便掌握讀 script 的時機，也可以用 onLoad 在讀取完後做事
+      strategy: beforeInteractive, afterInteractive, lazyOnload, worker 
+        beforeInteractive: 在網頁可以互動前讀取
+        afterInteractive: 在網頁可以互動後讀取(default)
+        lazyOnload: 網頁閒置時讀取，適合一些比較不急的script，等其他要fetch的資料都抓好後再來讀取
+        worker: 利用 web worker 來讀取 script (experimental)
+        see: https://nextjs.org/docs/basic-features/script#off-loading-scripts-to-a-web-worker-experimental
+      */}
+      <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() => { console.log('script loaded') }}
+      />
     </Head>
 
     <h1>First Post</h1>
