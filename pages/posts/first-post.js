0898c144a31d5d4b8b06e4e06ffe92c81548f047
roto93 <roto93@yahoo.com.tw>
2022-08-28 00:16:27 +0800

    :zap: add image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
   return <>
     <h1>First Post</h1>
     <Link href="/">Back to home</Link>
+
+    {/* 使用靜態圖片 */}
+    {/* 圖片必須放在 public 資料夾裡 */}
+    {/* Nextjs 會幫你 handle 圖片 resize 的問題，避免把過大的圖片傳送到小裝置上 */}
+    <Image
+      src={'/images/profile.jpg'}
+      width={250}
+      height={250}
+      alt={'example'}
+    />
   </>
-}
+} 
