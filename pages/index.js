07ea6c514d3f4dfef38ab3dd4e779a46db0153f6
roto93 <roto93@yahoo.com.tw>
2022-08-31 00:26:19 +0800

    :zap: getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-{/* 要改變Link的樣式，就要在裡面另外用<a>包住，然後在<a>加className */ }
-{/* 要連到外部連結，就直接用<a>*/ }
+// 要改變Link的樣式，就要在裡面另外用<a>包住，然後在<a>加className
+// 要連到外部連結，就直接用<a>
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+
+// 當要使用 SSG 時，可以使用 getStaticPorps，
+// getStaticProps 會在 build 時運行，也就是生成靜態的HTML前生成資料
+// 讓你要進行server中的非同步事件或抓外部資料都可以
+// 若想在 request time才生成資料，可使用 getServerSideProps
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +32,22 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   );
 }
